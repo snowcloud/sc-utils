4100bbff11f5b31c01608b0a04833d6ae812d015
snowcloud <derek@snowcloud.co.uk>
2010-10-25 22:17:13 +0100

    added get_current_user to middleware

--- scutils/middleware.py
+++ scutils/middleware.py
@@ -105,3 +105,19 @@ class Custom403Middleware(object):
              return forbidden(request)
           else:
              return response
+
+import threading
+
+_thread_locals = threading.local()
+
+def get_current_user():
+    try:
+        return _thread_locals.user
+    except AttributeError:
+        return None
+
+class ThreadLocals(object):
+	"""Middleware that adds various objects to thread local storage from the request object."""
+	def process_request(self, request):
+		_thread_locals.user = getattr(request, 'user', None)
+
