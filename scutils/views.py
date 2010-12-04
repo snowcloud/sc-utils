fc69fd685eb2908a2a3fd28e0e72163cf0037df3
snowcloud <derek@snowcloud.co.uk>
2010-12-04 01:26:15 +0000

    added code to secure link methods

--- scutils/views.py
+++ scutils/views.py
@@ -26,11 +26,12 @@ These are referenced from the setting TEMPLATE_CONTEXT_PROCESSORS and
 used by DjangoContext.
 """
 
+import time, hashlib, struct
+
 def gen_sec_link(uri_prefix, secret, rel_path):
     """returns url for lighttpd mod_secure_download
         eg /sdl/<token>/<hashtimestamp>/my_secure_doc.pdf
     """
-    import time, hashlib
     hextime = "%08x" % time.time()
     # NB rel_path must start with a slash
     if not rel_path.startswith('/'):
@@ -38,6 +39,26 @@ def gen_sec_link(uri_prefix, secret, rel_path):
     token = hashlib.md5(secret + rel_path + hextime).hexdigest()
     return '%s%s/%s%s' % (uri_prefix, token, hextime, rel_path)
 
+def check_sec_link(secret, token, hextime, rel_path):
+    """The time seems to be shortened by hex, so can't compare hextime with today
+        This will return true if the token matches the secret+hextime+rel_path
+        and the hextime is within the last 24 hours"""
+    if not rel_path.startswith('/'):
+        rel_path = '/%s' % rel_path
+    token_ok = (token == hashlib.md5(secret + rel_path + hextime).hexdigest())
+    in_time = False
+    if token_ok:
+        hexnow = "%08x" % time.time()
+        secs = struct.unpack('!f', hextime.decode('hex'))[0]
+        secsnow = struct.unpack('!f', hexnow.decode('hex'))[0]
+        then = time.gmtime(secs)
+        now = time.gmtime(secsnow)
+        diff =  time.gmtime(secsnow - secs)
+        # these are secs from epoch - 1970, 1, 1, ...
+        # this will check diff is less than 24 hrs
+        in_time = diff[0] == 1970 and diff[1] == 1 and diff[2] == 1
+    return token_ok, in_time
+    
 from django.shortcuts import render_to_response
 from django.template import TemplateDoesNotExist
 
