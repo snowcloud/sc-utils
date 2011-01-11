cbbd63d677fc69245572554897ad576133204d63
snowcloud <derek@snowcloud.co.uk>
2011-01-11 02:08:13 +0000

    fixed request parsing to just use referrer path

--- scutils/forms.py
+++ scutils/forms.py
@@ -68,8 +68,7 @@ class SimpleStaticSiteContactForm(ContactForm):
     def clean(self):
             cleaned_data = self.cleaned_data
             addr = self.request.META['SERVER_ADDR']
-            origin = self.request.META.get('HTTP_ORIGIN', None) or \
-                        self.request.META['HTTP_REFERER']
+            origin = self.request.META['HTTP_REFERER']
             from urlparse import urlparse
             h = urlparse(origin)
             origin = h.netloc
