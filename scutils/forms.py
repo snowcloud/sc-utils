743ffb6f3c52821036700202f12f656e119c08c5
snowcloud <derek@snowcloud.co.uk>
2011-01-25 17:06:30 +0000

    restored fixed bleach import

--- scutils/forms.py
+++ scutils/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from contact_form.forms import ContactForm, attrs_dict
 from django.conf import settings
 
-# from bleach import Bleach
+import bleach
 
 
 class SCContactForm(ContactForm):
@@ -52,36 +52,36 @@ class SimpleStaticSiteContactForm(ContactForm):
         #                                   self.get_context())
         return self.subject_txt
     
-    # def clean_name(self):
-    #     """docstring for clean_name"""
-    #     data = self.cleaned_data['name']
-    #     return Bleach().clean(self.cleaned_data['name'])
-    # 
-    # def clean_email(self):
-    #     data = self.cleaned_data['email']
-    #     return Bleach().clean(self.cleaned_data['email'])
-    # 
-    # def clean_body(self):
-    #     data = self.cleaned_data['body']
-    #     return Bleach().clean(self.cleaned_data['body'])
-    # 
-    # def clean(self):
-    #         cleaned_data = self.cleaned_data
-    #         addr = self.request.META['SERVER_ADDR']
-    #         origin = self.request.META['HTTP_REFERER']
-    #         from urlparse import urlparse
-    #         h = urlparse(origin)
-    #         origin = h.netloc
-    #                 
-    #         cred = settings.EXT_CONTACTMAIL.get(origin, None)
-    #     
-    #         if cred and addr in cred.get('ip', []):
-    #             # #     EXT_CONTACTMAIL = {
-    #             #         # HTTP_ORIGIN, REMOTE_ADDR
-    #             #         'http://www.ganzie.com': {'ip': ['127.0.0.1'], 'send_to': ['derek.hoy@gmail.com', 'derek@rarebits']},
-    #         
-    #             self.recipient_list = cred['send_to']
-    #             self.subject_txt = cred['subject']
-    #             return cleaned_data
-    #                     
-    #         raise forms.ValidationError('failed validation against settings')
+    def clean_name(self):
+        """docstring for clean_name"""
+        data = self.cleaned_data['name']
+        return bleach.clean(self.cleaned_data['name'])
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        return bleach.clean(self.cleaned_data['email'])
+    
+    def clean_body(self):
+        data = self.cleaned_data['body']
+        return bleach.clean(self.cleaned_data['body'])
+    
+    def clean(self):
+            cleaned_data = self.cleaned_data
+            addr = self.request.META['SERVER_ADDR']
+            origin = self.request.META['HTTP_REFERER']
+            from urlparse import urlparse
+            h = urlparse(origin)
+            origin = h.netloc
+                    
+            cred = settings.EXT_CONTACTMAIL.get(origin, None)
+        
+            if cred and addr in cred.get('ip', []):
+                # #     EXT_CONTACTMAIL = {
+                #         # HTTP_ORIGIN, REMOTE_ADDR
+                #         'http://www.ganzie.com': {'ip': ['127.0.0.1'], 'send_to': ['derek.hoy@gmail.com', 'derek@rarebits']},
+            
+                self.recipient_list = cred['send_to']
+                self.subject_txt = cred['subject']
+                return cleaned_data
+                        
+            raise forms.ValidationError('failed validation against settings')
