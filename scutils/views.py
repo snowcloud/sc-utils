16fd034d68cc0b3e9aa365ef2000ee75be8690a6
snowcloud <derek@snowcloud.co.uk>
2011-01-26 20:21:39 +0000

    removed last change- error was caused by bug in other module

--- scutils/views.py
+++ scutils/views.py
@@ -10,12 +10,9 @@ def set_vars(request):
     except AttributeError:
         allow_anon_users = False
     context_extras = {}
-    try:
-        context_extras['REQ_PATH'] = request.path
-        context_extras['REQ_FULL_PATH'] = request.get_full_path()
-        context_extras['ALLOW_ANON_USERS'] = allow_anon_users
-    except AttributeError:
-        pass
+    context_extras['REQ_PATH'] = request.path
+    context_extras['REQ_FULL_PATH'] = request.get_full_path()
+    context_extras['ALLOW_ANON_USERS'] = allow_anon_users
     return context_extras
 
 
